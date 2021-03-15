211504c82180be2253afca254f40cd139a4f724a
Welisson luca <welissonluca17@gmail.com>
2021-03-15 09:39:40 -0300

    feat: tests completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+	const user = users.find((user) => user.username === username);
+
+	if (!user) return response.status(404).json({ error: "user not found" });
+
+	request.user = user;
+
+	return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+ 
+  if(user.todos.length >= 10 && user.pro === false)
+         return response.status(403).json({error: "you have passed the task limit consider migrating to the pro plan"});
+  
+  return next();
+ 
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if(!validate(id))
+    return response.status(400).json({error: "Invalid todo identifier"});
+
+  const user = users.find(user => user.username === username);
+  
+  if(!user)
+   return response.status(404).json({error: "use not found"});
+
+   const todo = user.todos.find(todo => todo.id === id)
+  
+   if(!todo)
+    return response.status(404).json({error: "Todo not found"});
+  
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+	const user = users.find( user => user.id === id);
+
+	if (!user) return response.status(404).json({ error: "user not foun" });
+
+	request.user = user;
+
+	return next();
+
 }
 
 app.post('/users', (request, response) => {
@@ -117,7 +161,7 @@ app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, re
 
   user.todos.splice(todoIndex, 1);
 
-  return response.status(204).send();
+  return response.status(204).send();''
 });
 
 module.exports = {
